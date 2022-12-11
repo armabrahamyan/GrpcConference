34fc9ec23c9ae49676354fc5887949ec651fb95d
DELL2IN1\armen <armena1998@yahoo.com>
2022-12-11 22:53:10 +0400

    Add project files.

---
+++ GrpcClientFaultHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Configuration;
+using static GrpcService.Greeter;
+
+
+var defaultMethodConfig = new MethodConfig
+{
+    Names = { MethodName.Default },
+    RetryPolicy = new RetryPolicy
+    {
+        MaxAttempts = 5,
+        InitialBackoff = TimeSpan.FromSeconds(1),
+        MaxBackoff = TimeSpan.FromSeconds(5),
+        BackoffMultiplier = 1.5,
+        RetryableStatusCodes = { StatusCode.Unavailable }
+    }
+};
+
+
+var channel = GrpcChannel.ForAddress("https://localhost:7247", new GrpcChannelOptions
+{
+    ServiceConfig = new ServiceConfig { MethodConfigs = { defaultMethodConfig } }
+});
+var client = new GreeterClient(channel);
+    
+var reply = await client.SayHelloAsync(new GrpcService.HelloRequest
+{
+    Name = "Purlin",
+});
+
+Console.WriteLine($"Reply from server {reply.Message}");
---
+++ GrpcClientLoadBalancing/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client.Balancer;
+using Grpc.Net.Client;
+using Microsoft.Extensions.DependencyInjection;
+using GrpcService;
+using Grpc.Net.Client.Configuration;
+
+var factory = new StaticResolverFactory(addr => new[]
+{
+    new BalancerAddress("localhost", 7247),
+    new BalancerAddress("localhost", 7248)
+});
+
+var services = new ServiceCollection();
+services.AddSingleton<ResolverFactory>(factory);
+
+var channel = GrpcChannel.ForAddress(
+    "static:///localhost",
+    new GrpcChannelOptions
+    {
+        Credentials = ChannelCredentials.SecureSsl,
+        ServiceConfig = new ServiceConfig { LoadBalancingConfigs = { new RoundRobinConfig() } },
+        ServiceProvider = services.BuildServiceProvider()
+    });
+var client = new Greeter.GreeterClient(channel);
+
+
+for (int i = 1; i < 10; i++)
+{
+    var reply = await client.SayHelloAsync(new HelloRequest
+    {
+        Name = "Purlin co.",
+    });
+
+    Console.WriteLine(reply.Message + $"request N: {i}");
+}
+    
